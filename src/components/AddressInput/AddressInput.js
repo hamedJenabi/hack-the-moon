066f5c4beaafcc2af5beea882b687b6dad61aca4
AutMaxHoffmann <max.hoffmann@gmail.com>
2023-11-11 18:29:21 +0100

    feat: add AddressInput

---
+++ src/components/AddressInput/AddressInput.js
@@ -0,0 +1,57 @@
+'use client'
+
+import styles from "./AddressInput.module.scss";
+import { useEffect, useState ,startTransition, useMemo,} from "react";
+import debounce from 'lodash/debounce';
+
+import * as Ariakit from "@ariakit/react";
+
+
+export default function AddressInput() {
+    const [locations, setLocations] = useState([]);
+
+    const [searchValue, setSearchValue] = useState("");
+
+    useEffect(() => {
+      const fetchLocations = async () => {
+        const url = `https://api.mapbox.com/search/searchbox/v1/suggest?q=${searchValue}&language=de&country=at&types=place&session_token=0bf6e88b-28b0-4e18-8bbe-c76818011465&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`;
+
+        await fetch(url).then((response) =>
+          response.text()).then((res) => JSON.parse(res))
+        .then((json) => {
+          setLocations(json?.suggestions?.filter(f => f?.context?.region?.region_code == 4));
+        console.log(json.suggestions);
+        }).catch((err) => console.log({ err }));
+      };
+      fetchLocations();
+    }, [searchValue]);
+
+
+    return (
+    <main className={styles.main}>
+   
+      <Ariakit.ComboboxProvider
+        setValue={(value) => {
+          startTransition(() => setSearchValue(value));
+        }}
+      >
+
+          <Ariakit.Combobox placeholder="Ried im Innkreis" className={styles.combobox} />
+     
+        <Ariakit.ComboboxPopover gutter={8} sameWidth className={styles.popover}>
+          {locations?.length ? (
+            locations?.map((value) => (
+              <Ariakit.ComboboxItem
+                key={value.name}
+                value={value.name}
+                className={styles.comboboxItem}
+              />
+            ))
+          ) : (
+            <div className={styles.noResults}>No results found</div>
+          )}
+        </Ariakit.ComboboxPopover>
+      </Ariakit.ComboboxProvider>
+  
+    </main>);
+  }
