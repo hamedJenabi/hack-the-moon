e2eb678f1c092744e9e2c18685ffae4c37fba347
Hamed <hamed.jenabi@gmail.com>
2023-11-11 15:48:37 +0100

    feat: buttons

---
+++ src/app/components/Button/Button.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./Button.module.scss";
+import Image from "next/image";
+import classNames from "classnames";
+
+function Button({ onClick, children, iconOnly }) {
+  return (
+    <button
+      className={classNames(styles.btn, {
+        [styles.iconOnlyWrapper]: iconOnly,
+      })}
+      onClick={onClick}
+    >
+      <div
+        className={classNames(styles.default, { [styles.iconOnly]: iconOnly })}
+      >
+        <Image
+          alt="go back"
+          className={styles.leftIcon}
+          src={iconOnly ? "/chevron_left-black.svg" : "/chevron_left.svg"}
+          width={iconOnly ? 20 : 30}
+          height={iconOnly ? 20 : 30}
+        />
+      </div>
+      {!iconOnly && children}
+      {!iconOnly && (
+        <Image
+          className={styles.rightIcon}
+          alt="go back"
+          src="/chevron_right.svg"
+          width={iconOnly ? 20 : 30}
+          height={iconOnly ? 20 : 30}
+        />
+      )}
+    </button>
+  );
+}
+
+export default Button;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import Button from "./components/Button/Button";
 
 export default function Home() {
-  return <main className={styles.main}>initial</main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>initial</div>
+    </main>
+  );
 }
