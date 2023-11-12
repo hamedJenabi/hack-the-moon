cb259bd2638ba7c42a31c48b58cbf25cdbc75255
AutMaxHoffmann <max.hoffmann@gmail.com>
2023-11-12 10:52:03 +0100

    feat: add loading animation

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -4,21 +4,27 @@ import React, { useEffect, useState } from 'react'
 import styles from './Overview.module.scss'
 import OverviewCard from './OverviewCard';
 import { Card } from '@/components';
+import LottieAnimation from '../LottieAnimation/Lottie';
 
 const Overview = ({ results }) => {
   const [data, setData] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchResults = async () => {
       const url = `https://moon.neon.zone/api/schedules?token=nJDknmuPRbUEXZ5B`;
 
+      setIsLoading(true);
       await fetch(url, {
         method: "POST",
         headers: { 'accept': 'application/json', 'content-type': 'application/json' },
         body: JSON.stringify(Object.assign({}, ...results))
       },).then((response) =>
         response.text()).then((res) => JSON.parse(res))
-        .then((json) => setData(json.data)).catch((err) => console.log({ err }));
+        .then((json) => {
+          setIsLoading(false);
+          setData(json.data);
+        }).catch((err) => console.log({ err }));
     };
 
     fetchResults();
@@ -29,9 +35,13 @@ const Overview = ({ results }) => {
     <div className={styles.wrapper}>
       <h1>See what we have prepared for you</h1>
       <div className={styles.overviewCardWrapper}>
-        {data?.items?.length ? data?.items?.map((el) => (
+        {data?.items?.length && data?.items?.map((el) => (
           <OverviewCard key={el.id} el={el?.poi} startDate={el.startTime} endDate={el.endTime} />
-        )) : <Card>Results not found</Card>}
+        ))}
+         {!data?.items?.length && !isLoading && <Card>Results not found</Card>}
+
+        {isLoading && <LottieAnimation/>}
+
       </div>
     </div>
   )
